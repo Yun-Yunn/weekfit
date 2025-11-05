f724c8dc123de59ce9c0cae45a94071c2bdca78c
GalakX2 <goupy.jordan@gmail.com>
2025-11-05 10:08:10 +0100

    .env + migrate

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,20 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExerciseController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\StatsController;
+use Inertia\Inertia;
+use Laravel\Fortify\Features;
 
+Route::get('/', function () {
+    return Inertia::render('welcome', [
+        'canRegister' => Features::enabled(Features::registration()),
+    ]);
+})->name('home');
 
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/exercises', [ExerciseController::class, 'index'])->name('exercises.index');
