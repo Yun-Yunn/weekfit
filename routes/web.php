c95c71ed91cf77c0a9f76fe3463884c5ea1792ea
GalakX2 <goupy.jordan@gmail.com>
2025-11-05 11:06:17 +0100

    Register OK

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,27 @@
 <?php
 
-// use App\Http\Controllers\ProfileController;
-// use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ExerciseController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\StatsController;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
-})->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
 
-// Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/exercises', [ExerciseController::class, 'index'])->name('exercises.index');
-Route::get('/stats', [StatsController::class, 'index'])->name('stats.index');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
